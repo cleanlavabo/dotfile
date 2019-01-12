5bb66ccaf929b43d0806edc26f5197815e727a57
Bee <11498224+cleanlavabo@users.noreply.github.com>
2019-01-12 16:35:58 +0700

    Update zsh-config.sh

--- zsh-config.sh
+++ zsh-config.sh
@@ -1,6 +1,7 @@
 # If you come from bash you might have to change your $PATH.
 ################# source "$HOME/.vim/gruvbox_256palette.sh"
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
+export TERM="xterm-256color"
 
 # Path to your oh-my-zsh installation.
   export ZSH="/Users/phatnguyen/.oh-my-zsh"
