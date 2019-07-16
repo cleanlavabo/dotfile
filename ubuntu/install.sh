164e4ca15238745e34f44c2604783ba67394b88e
lazybee27102 <lazybee27102@gmail.com>
2019-07-16 17:49:21 +0700

    Add docker for ubuntu

---
+++ ubuntu/install.sh
@@ -0,0 +1,14 @@
+sudo su
+yes | apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+yes | install docker-ce docker-ce-cli containerd.io
+
