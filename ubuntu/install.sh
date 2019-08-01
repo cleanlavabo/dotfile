8e6406186b37ca0e16c01189689aca424bc0a0db
Phat Nguyen <hoangphat@inspectorio.com>
2019-08-01 13:37:19 +0700

    Instruction to connect airpod with ubuntu 18.04LTS

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,21 +1,8 @@
-sudo su
-yes | apt install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-yes | install docker-ce docker-ce-cli containerd.io
+apt update && apt upgrade -y
+apt install git wget curl zsh vim -y
 
-yes | apt install wget ca-certificates
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
-sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-
-apt update
-apt install postgresql postgresql-contrib
+# docker
+# curl -fsSL https://get.docker.com -o get-docker.sh
+# sh get-docker.sh
+# docker run hello-world
